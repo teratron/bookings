f27c204d1a170d53311bbf4b759e0282e68d0f80
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-05-16 12:09:18 +0300

    Add router.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 
 import Spinner from '../Spinner';
 import Header from '../Header';
@@ -10,11 +11,50 @@ function App() {
     return (
         <React.Suspense fallback={<Spinner/>}>
             <div className="app">
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                </nav>
                 <Header/>
                 <Main/>
                 <Footer/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="about" element={<About/>}/>
+                </Routes>
             </div>
         </React.Suspense>
+    );
+}
+
+function Home() {
+    return (
+        <>
+            <main>
+                <h2>Welcome to the homepage!</h2>
+                <p>You can do this, I believe in you.</p>
+            </main>
+            <nav>
+                <Link to="/about">About</Link>
+            </nav>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <main>
+                <h2>Who are we?</h2>
+                <p>
+                    That feels like an existential question, don't you
+                    think?
+                </p>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </>
     );
 }
 
