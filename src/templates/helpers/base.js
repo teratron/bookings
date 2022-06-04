5ce2c390fffa8c58a144d032e935db138174f7c4
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-06-04 20:30:48 +0300

    Bootstrap

--- src/templates/helpers/base.js
+++
@@ -1,7 +0,0 @@
-import Handlebars from 'handlebars';
-
-const context = {
-    title: ' - HEADER****************'
-}
-
-export default Handlebars.compile('{{context}}')
--- src/templates/helpers/header.js
+++
@@ -1,8 +0,0 @@
-import Handlebars from 'handlebars';
-import template from '../partials/header.handlebars'
-
-const context = {
-    title: ' - HEADER'
-}
-
-export default Handlebars.compile(template(context))
