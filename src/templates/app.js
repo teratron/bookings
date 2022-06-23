c5ce680023f6b8ae4e94596dcc298afb1a15000b
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-06-23 17:17:56 +0300

    Surender.

--- src/templates/app.js
+++ src/templates/app.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import packageJSON from '../../package.json'
 
-
 export const props = {
     title: 'Bookings',
     description: 'Bookings - hotels etc.',
@@ -10,5 +9,23 @@ export const props = {
     version: packageJSON.version,
     pages: fs.readdirSync('./src/templates/pages')
         .filter(fileName => fileName.endsWith('.js'))
-        .map(page => page.replace(/.js/gi, ''))
+        .map(page => page.replace(/.js/gi, '')),
+    page: function () {
+        const list = fs.readdirSync('./src/templates/pages')
+            .filter(fileName => fileName.endsWith('.js'))
+            .map(page => page.replace(/.js/gi, ''))
+
+        let page = {}
+
+        for (const key of list) {
+            let val = key
+            if (key === 'index') val = 'home'
+            page[key] = {
+                title: val.replace(/[_|-]/, ' ').trim(),
+                url: `./${val}.html`
+            }
+        }
+
+        return page
+    }()
 }
